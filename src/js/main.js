4b324b8cba92f27c1708e37e921bb0eb818a3886
robbsnor1 <robbinschmidt@gmail.com>
2020-01-12 21:53:50 +0100

    start

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 
-console.log('main')
+
+$(document).ready(function () {
+
+
+  $(document).on('click', '.input-group--number .arrows svg', function () {
+
+    var inputField = $(this).parents('.input-group').find('.form-control');
+
+    var elementVal = inputField.val();    
+
+
+    if ($(this).hasClass('add')) {
+      var x =  elementVal++
+      inputField.val(elementVal++)
+    } else {
+      var x = elementVal--
+      inputField.val(elementVal--)
+    }
+  })
+
+
+});
