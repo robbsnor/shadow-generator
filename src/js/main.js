ed0068e8b68fa5c62866c4abe59c4cabb580d923
Robbsnor <robbinschmidt@gmail.com>
2020-01-13 10:25:19 +0100

    increase fix pat <3

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,21 @@
 $(document).ready(function () {
 
 
+
+  
+
   $(document).on('click', '.input-group--number .arrows svg', function () {
 
-    var inputField = $(this).parents('.input-group').find('.form-control');
+    var inputField = $(this).parents('.input-group').find('.form-control');  
+    var elementVal = inputField.val()
 
-    var elementVal = inputField.val();    
-
-
+    
     if ($(this).hasClass('add')) {
-      var x =  elementVal++
-      inputField.val(elementVal++)
+      elementVal ++
+      inputField.val(elementVal)
     } else {
-      var x = elementVal--
-      inputField.val(elementVal--)
+      elementVal --
+      inputField.val(elementVal)
     }
   })
 
