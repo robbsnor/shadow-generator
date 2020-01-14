662284df87b55bf857830daea50a3990ee39ec63
robbsnor1 <robbinschmidt@gmail.com>
2020-01-14 22:27:19 +0100

    1 slider

--- src/js/box-shadow.js
+++ src/js/box-shadow.js
@@ -1,93 +1,27 @@
-function isInset() {
-  if (document.getElementById('inset').checked) {
-    return 'inset';
-  } else {
-    return '';
+
+// var horizontalOffset = 0;
+
+
+// range sliders
+$('.setting input[type="range"]').rangeslider({
+  polyfill: false,
+
+  onInit: function () {
+  },
+
+  onSlide: function (position, value) {
+    var parent = $(this.$element).parent('.setting');
+    parent.find('input').val(value)
   }
-}
-
-function checkEmpty(val) {
-  if (val == '') {
-    return 0;
-  } else {
-    return val;
-  }
-}
-
-function getShadowVals() {
-
-  boxShadowVals = '';
-
-  var boxShadowSettings = {
-    inset: isInset(),
-    horizontalOffset: checkEmpty($('#horizontal-offset').val()) + 'px',
-    verticalOffset: checkEmpty($('#vertical-offset').val()) + 'px',
-    blurRadius: checkEmpty($('#blur-radius').val()) + 'px',
-    spreadRadius: checkEmpty($('#spread-radius').val()) + 'px',
-    shadowColor: $('#shadow-color').css('background-color'),
-
-    // boxColor: $('#box-color').css('background-color'),
-  }
+});
 
 
-  for (key in boxShadowSettings) {
-    // console.log(key)
-    // console.log(boxShadowSettings[key])
-    boxShadowVals = boxShadowVals + (boxShadowSettings[key] + ' ');
-  }
-
-  $('.final-preview').html(boxShadowVals).css({
-    'box-shadow': boxShadowVals
-  });
-}
+$('input[type="number"]').keypress(function () {
+});
 
 
 
-$(document).on('click', function () {
-  getShadowVals()
+$('body').on('click', function () {
+      $('input[type="range"]').val($('input').val()).change();
 })
-getShadowVals()
 
-
-
-
-
-function initColorPicker(element) {
-
-  var boxShadowPicerElement = document.querySelector(element);
-  var boxShadowPicer = new Picker({
-    parent: boxShadowPicerElement,
-    color: boxShadowPicerElement.style.background
-  });
-  boxShadowPicer.onChange = function (color) {
-    boxShadowPicerElement.style.background = color.rgbaString;
-  };
-}
-
-initColorPicker('#shadow-color')
-initColorPicker('#box-color')
-
-
-
-// $('.preview').html(boxShadowVals).css({
-//   'box-shadow': boxShadowVals
-// });
-
-// function checkHash(val) {
-//   console.log(val)
-//   if (!val.startsWith('#')) {
-//     val = '#' + val;
-//     $('.color').css({
-//       'background': val
-//     });
-//     $('#shadow-color').val(val);
-
-//     return val;
-//   } else {
-//     $('.color').css({
-//       'background': val
-//     });
-
-//     return val;
-//   }
-// }
--- src/js/main.js
+++ src/js/main.js
@@ -25,4 +25,5 @@ $(document).ready(function () {
       input.val(val);
     })
   })
+  
 });
